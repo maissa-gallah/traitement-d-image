af0899fceb49a30e53731bdce5269c9925afe3f0
shatha siala <shatha.siala@gmail.com>
2022-04-09 14:59:26 +0100

    Added median filter

--- filtre_moyen_filtre_median/filtre.py
+++ filtre_moyen_filtre_median/filtre.py
@@ -12,10 +12,10 @@ def remplirbourdureimg(img,lx,ly,n):
     t[:, 0:a] = np.flip(t[:, a+1:2*a+1],axis=1)
     # padding mirror right
     t[:, lx+a:lx+2*a] =  np.flip(t[:, lx-1:lx+a-1],axis=1)
-    return(t,lx,ly)
+    return t
   
 def moyennefilter(img,lx,ly,n):
-    (t,lx,ly)=remplirbourdureimg(img,lx,ly,n)
+    t =remplirbourdureimg(img,lx,ly,n)
     a=n//2
     imgm=img
     new_img=img
@@ -24,6 +24,18 @@ def moyennefilter(img,lx,ly,n):
             arr=t[i-a:i+a+1,j-a:j+a+1]
             imgm[i-a][j-a]=np.average(arr)
     return imgm
+
+def medianFiltre(img, lx, ly, n):
+    new_img = img
+    a= n//2
+    img_filled = remplirbourdureimg(img, lx, ly, n)
+
+    for i in range (a, ly+a):
+        for j in range(a, lx+a):
+            arr=img_filled[i-a:i+a+1,j-a:j+a+1]
+            print(i, ",",j)
+            new_img[i-a][j-a] = np.median(arr)
+    return new_img
 
 
 
--- filtre_moyen_filtre_median/main.py
+++ filtre_moyen_filtre_median/main.py
@@ -3,17 +3,17 @@ import cv2 as cv
 import sys
 
 
-sys.path.append("D:/desktopMaissa/gl4/S2/Traitement d'image/TPGL4/tps/")
+sys.path.append("C:/Users/LENOVO/Desktop/GL4/semestre 2/Traitement d'images/traitement-d-image/")
 
 from filtre_moyen_filtre_median.bruit import bruit
 from read_write_histogram.writePGM import pgmwrite
 from  read_write_histogram.readPGM import readpgm
-from filtre_moyen_filtre_median.filtre import moyennefilter
-path="D:/desktopMaissa/gl4/S2/Traitement d'image/TPGL4/tps/images/"
+from filtre_moyen_filtre_median.filtre import medianFiltre, moyennefilter
+path="C:/Users/LENOVO/Desktop/GL4/semestre 2/Traitement d'images/traitement-d-image/images/"
 
-img1 = cv.imread(path+'example.pgm') 
+img1 = cv.imread(path+'coins.pgm') 
 cv.imshow("Test", img1)
-img,lx,ly=readpgm(path+'example.pgm')
+img,lx,ly=readpgm(path+'coins.pgm')
 imgb=bruit(img,lx,ly)
 
 pgmwrite(imgb, path+"bruit.pgm",lx,ly)
@@ -24,6 +24,12 @@ imgm=moyennefilter(img,lx,ly,5)
 pgmwrite(imgm, path+"filtermoyenne.pgm",lx,ly)
 imgfiltermoy = cv.imread(path+"filtermoyenne.pgm") 
 cv.imshow("filtermoyenne.pgm", imgfiltermoy)
+
+new_img = medianFiltre(imgb,lx,ly,3)
+
+pgmwrite(new_img, path+"new_img.pgm",lx,ly)
+new_img_file = cv.imread(path+"new_img.pgm") 
+cv.imshow("Median Filter", new_img_file)
 
 
 cv.waitKey()
