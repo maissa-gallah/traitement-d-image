eda5b78be9c93741da21a4185902b8d886cc779a
maissa-gallah <maissaglh@gmail.com>
2022-04-09 15:49:01 +0200

    filter moyenne

---
+++ filtre_moyen_filtre_median/filtre.py
@@ -0,0 +1,30 @@
+import numpy as np
+def remplirbourdureimg(img,lx,ly,n):
+    imgb =img
+    a=n//2
+    t=np.zeros((ly+n-1,lx+n-1))
+    t[a:ly+a , a:lx+a]=img
+    # padding mirror top
+    t[0:a, a:a+lx] = np.flip(img[1:a+1, 0:lx],axis=0)
+    # padding mirror bottom
+    t[ly+a:ly+2*a, a:lx+a]= np.flip(img[ly-a-1:ly-1, 0:lx],axis=0)
+    # padding mirror left
+    t[:, 0:a] = np.flip(t[:, a+1:2*a+1],axis=1)
+    # padding mirror right
+    t[:, lx+a:lx+2*a] =  np.flip(t[:, lx-1:lx+a-1],axis=1)
+    return(t,lx,ly)
+  
+def moyennefilter(img,lx,ly,n):
+    (t,lx,ly)=remplirbourdureimg(img,lx,ly,n)
+    a=n//2
+    imgm=img
+    new_img=img
+    for i in range (a,ly+a):
+        for j in range(a,lx+a):
+            arr=t[i-a:i+a+1,j-a:j+a+1]
+            imgm[i-a][j-a]=np.average(arr)
+    return imgm
+
+
+
+
--- filtre_moyen_filtre_median/main.py
+++ filtre_moyen_filtre_median/main.py
@@ -1,11 +1,14 @@
 
 import cv2 as cv
 import sys
+
+
 sys.path.append("D:/desktopMaissa/gl4/S2/Traitement d'image/TPGL4/tps/")
 
 from filtre_moyen_filtre_median.bruit import bruit
 from read_write_histogram.writePGM import pgmwrite
 from  read_write_histogram.readPGM import readpgm
+from filtre_moyen_filtre_median.filtre import moyennefilter
 path="D:/desktopMaissa/gl4/S2/Traitement d'image/TPGL4/tps/images/"
 
 img1 = cv.imread(path+'example.pgm') 
@@ -16,5 +19,11 @@ imgb=bruit(img,lx,ly)
 pgmwrite(imgb, path+"bruit.pgm",lx,ly)
 imgbruit = cv.imread(path+"bruit.pgm") 
 cv.imshow("bruit.pgm", imgbruit)
+
+imgm=moyennefilter(img,lx,ly,5)
+pgmwrite(imgm, path+"filtermoyenne.pgm",lx,ly)
+imgfiltermoy = cv.imread(path+"filtermoyenne.pgm") 
+cv.imshow("filtermoyenne.pgm", imgfiltermoy)
+
 
 cv.waitKey()
