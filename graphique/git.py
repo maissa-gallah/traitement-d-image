ca0ff2cb5c1744b6b2ad365cfb8b30d79d262f19
maissa-gallah <maissaglh@gmail.com>
2022-05-29 20:10:49 +0200

    mmm

--- graphique/git.py
+++ graphique/git.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Başlangıç Tarihi: 06.05.2021 - 17:06
-# Bitiş Tarihi: 11.05.2021 - 21:00
-# instagram.com/yazilimfuryasi
-# yazilimfuryasi.com
-
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
@@ -12,6 +5,12 @@ from cv2 import cv2
 import numpy as np
 import threading
 import webbrowser
+import sys
+
+sys.path.append("D:/desktopMaissa/gl4/S2/Traitement d'image/TPGL4/tps")
+
+from  read_write_histogram.readPGM import readPGM
+from filtre_moyen_filtre_median.bruit import bruit
 
 tk = Tk()
 windowWidth = tk.winfo_reqwidth()
@@ -22,51 +21,45 @@ tk.geometry(f"800x510+{positionRight}+{positionDown}")
 tk.resizable(width=False, height=False)
 
 
-tk.title("Fotoğraf Maskesi Oluşturma ve Bulanıklaştırma | Yazılım Furyası")
+tk.title("Image Processing Tool")
 F1 = Frame(tk)
 F1.grid(row=0, column=0,pady=25, padx=25)
-l1 = Label(F1, text="Orijinal Resim", font="bold")
+l1 = Label(F1, text="Original Image", font="bold")
 l1.grid(row=0, column=0)
-L1 = Label(F1, text="Resim Alanı",height="25",width="52",bd=0.5, relief="solid")
+L1 = Label(F1, text="Original Image",height="25",width="52",bd=0.5, relief="solid")
 L1.grid(row=1, column=0, pady=10, padx=15)
-l2 = Label(F1, text="Maske", font="bold")
+l2 = Label(F1, text="Modified Image", font="bold")
 l2.grid(row=0, column=1)
-L2 = Label(F1, text="Maske",height="25",width="52",bd=0.5, relief="solid")
+L2 = Label(F1, text="Modified Image",height="25",width="52",bd=0.5, relief="solid")
 L2.grid(row=1, column=1)
 F2 = None
 F3 = None
 
-def Resim_Ekle():
+def add_image():
     global hsv
     global tkimage
-    global orjinal
+    global original
     global img_rgb
-    global resimselect
-    # Resim Seç
-    resimselect = filedialog.askopenfilename(initialdir = "Desktop")
-    # Resim Seçilmezse Geri Dönder
-    if not resimselect:
+    global selected_image
+    selected_image = filedialog.askopenfilename(initialdir = "Desktop")
+    # Returns If No Image Selected
+    if not selected_image:
         return
-    print(resimselect)
+    print(selected_image)
 
     try:
-        # Resimi Oku
-          
-        orjinal = cv2.imread(resimselect)
-
-        # Resimi Aç
-        
-        im = Image.fromarray(orjinal)
+        original = cv2.imread(selected_image)
+        # Open Image
+        im = Image.fromarray(original)
         im.thumbnail((360, 360))
         tkimage = ImageTk.PhotoImage(im)
-
         img_rgb = np.array(im)
 
-        # RGB'den BGR'ye dönüştürürken kullanılan parametre cv2.COLOR_RGB2BGR
+        # Parameter used when converting from RGB to BGR cv2.COLOR_RGB2BGR
         hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
 
-        # Widgetler
-        L1 = Label(F1, image = None) #, image = tkimage)
+        # Widget
+        L1 = Label(F1, image = None)
         L1.config(image = tkimage)
         
         L2 = Label(F1, image = None)
@@ -91,7 +84,7 @@ v9 = DoubleVar()
 v10 = DoubleVar()
 v11 = DoubleVar()
 def updateValue(event):
-    global k_resim
+    global k_picture
     global orjn
 
     saveBTN.config(state="disabled",cursor="")
@@ -99,20 +92,19 @@ def updateValue(event):
     low = np.array([l_h.get(), l_s.get(), l_v.get()])
     high = np.array([u_h.get(), u_s.get(), u_v.get()])
 
-    k_resim = cv2.inRange(hsv, low, high)
-    orjn = cv2.inRange(orjinal, low, high)
+    k_picture = cv2.inRange(hsv, low, high)
+    orjn = cv2.inRange(original, low, high)
 
-    imgtk3 = ImageTk.PhotoImage(image=Image.fromarray(k_resim))
+    imgtk3 = ImageTk.PhotoImage(image=Image.fromarray(k_picture))
     LabelHSV["text"] = ([l_h.get(),l_s.get(),l_v.get()],[u_h.get(),u_s.get(),u_v.get()])
 
     L2 = Label(F1, image = imgtk3)
     L2.image = imgtk3
     L2.grid(row=1, column=1)
 
-    # print([l_h.get(),l_s.get(),l_v.get()],[u_h.get(),u_s.get(),u_v.get()])
     saveBTN.config(state="normal",cursor="hand2")
 
-def yeniSayfa():
+def new_page():
     global l_h
     global l_s
     global l_v
@@ -127,51 +119,51 @@ def yeniSayfa():
     F2 = Frame(tk)
     F2.place(x=900,y=50)
     
-    l_h_lbl = Label(F2,text="Lower - H | Ton")
+    l_h_lbl = Label(F2,text="Lower - H | Tone")
     l_h_lbl.grid(row=0,column=0, sticky=W, padx="100")
     l_h = Scale(F2, length=255,variable = v1, from_ = 0, to = 179, troughcolor="white", orient = HORIZONTAL)
     l_h.bind("<ButtonRelease-1>", updateValue)
     l_h.grid(row=1,column=0)
-    l_s_lbl = Label(F2,text="Lower - S | Doygunluk")
+    l_s_lbl = Label(F2,text="Lower - S | Saturation")
     l_s_lbl.grid(row=2,column=0,sticky=W, padx="100")
     l_s = Scale(F2, length=255,variable = v2, from_ = 0, to = 255, troughcolor="white", orient = HORIZONTAL)
     l_s.bind("<ButtonRelease-1>", updateValue)
     l_s.grid(row=3,column=0)
-    l_v_lbl = Label(F2,text="Lower - V | Değer")
+    l_v_lbl = Label(F2,text="Lower - V | Value")
     l_v_lbl.grid(row=4,column=0,sticky=W, padx="100")
     l_v = Scale(F2, length=255,variable = v3, from_ = 0, to = 255, troughcolor="white",orient = HORIZONTAL)
     l_v.bind("<ButtonRelease-1>", updateValue)
     l_v.grid(row=5,column=0)
 
-    u_h_lbl = Label(F2,text="Upper - H | Ton")
+    u_h_lbl = Label(F2,text="Upper - H | Tone")
     u_h_lbl.grid(row=6,column=0, sticky=W, padx="100")
     u_h = Scale(F2, length=255,variable = v4, from_ = 0, to = 179, troughcolor="red", orient = HORIZONTAL)
     u_h.bind("<ButtonRelease-1>", updateValue)
     u_h.grid(row=7,column=0)
-    u_s_lbl = Label(F2,text="Upper - S | Doygunluk")
+    u_s_lbl = Label(F2,text="Upper - S | Saturation")
     u_s_lbl.grid(row=8,column=0,sticky=W, padx="100")
     u_s = Scale(F2, length=255,variable = v5, from_ = 0, to = 255, troughcolor="red", orient = HORIZONTAL)
     u_s.bind("<ButtonRelease-1>", updateValue)
     u_s.grid(row=9,column=0)
-    u_v_lbl = Label(F2,text="Upper - V | Değer")
+    u_v_lbl = Label(F2,text="Upper - V | Value")
     u_v_lbl.grid(row=10,column=0,sticky=W, padx="100")
     u_v = Scale(F2, length=255,variable = v6, from_ = 0, to = 255, troughcolor="red", orient = HORIZONTAL)
     u_v.bind("<ButtonRelease-1>", updateValue)
     u_v.grid(row=11,column=0)
-    reset = Button(F2, text ="Sıfırla",font="bold",fg="white", width = 10, command = Sıfırla, bg = "red")
+    reset = Button(F2, text ="Reset",font="bold",fg="white", width = 10, command = Reset, bg = "red")
     reset.config(cursor="hand2")
     reset.grid(row=13,column=0)
 
-    def Kopyala(event):
+    def Copy(event):
         hsv_deger = [l_h.get(),l_s.get(),l_v.get()],[u_h.get(),u_s.get(),u_v.get()]
         tk.clipboard_append(hsv_deger)
-        KayıtMesajı = Label(F2, text="Kopyalandı.", font="bold", fg="red", width = 14,height=2)
-        KayıtMesajı.grid(row=13,column=0)
-        KayıtMesajı.after(2000, KayıtMesajı.destroy)
+        RecordMesajı = Label(F2, text="Copied.", font="bold", fg="red", width = 14,height=2)
+        RecordMesajı.grid(row=13,column=0)
+        RecordMesajı.after(2000, RecordMesajı.destroy)
     LabelHSV = Label(F2,font="bold")
     LabelHSV.grid(row=12,column=0,pady=5)
     LabelHSV.config(cursor="hand2")
-    LabelHSV.bind("<Button-1>", Kopyala)
+    LabelHSV.bind("<Button-1>", Copy)
 
 def blur_def():
     global F3
@@ -230,7 +222,7 @@ def blrr2():
     saveBTN.config(state="disabled",cursor="")
 
     blurring = cv2.blur(img_rgb,(blurred.get(),blurred2.get()))
-    orjn = cv2.blur(orjinal,(blurred.get(),blurred2.get()))
+    orjn = cv2.blur(original,(blurred.get(),blurred2.get()))
     
     imgtk3 = ImageTk.PhotoImage(image=Image.fromarray(blurring))
 
@@ -247,7 +239,7 @@ def blrr():
     saveBTN.config(state="disabled",cursor="")
 
     bilFilter = cv2.bilateralFilter(img_rgb,blur.get(),blur2.get(),blur3.get())
-    orjn = cv2.bilateralFilter(orjinal,blur.get(),blur2.get(),blur3.get())
+    orjn = cv2.bilateralFilter(original,blur.get(),blur2.get(),blur3.get())
     
     imgtk3 = ImageTk.PhotoImage(image=Image.fromarray(bilFilter))
 
@@ -284,7 +276,7 @@ def blurring():
     blur2.bind("<ButtonRelease-1>", trgt_scale)
     blur2.grid(row=3,column=0,padx=27)
 
-def Sıfırla():
+def Reset():
     LabelHSV["text"] = ""
     L2 = Label(F1, image = tkimage)
     L2.image = tkimage
@@ -296,52 +288,60 @@ def Sıfırla():
     u_s.set(0)
     u_v.set(0)
 
-def Kayıt():
-    # Yeni Resmi Kayıt Et
+def Record():
+    # Record new image
     dosyaAdi = filedialog.asksaveasfilename(initialdir ="Desktop")
     if not dosyaAdi:
         return
-    KayıtMesajı = Label(F1, text="Kayıt Edildi.", font="bold")
-    KayıtMesajı.grid(row=2, column=1,pady=27)
-    KayıtMesajı.after(2000, KayıtMesajı.destroy)
+    RecordMesajı = Label(F1, text="Record Done.", font="bold")
+    RecordMesajı.grid(row=2, column=1,pady=27)
+    RecordMesajı.after(2000, RecordMesajı.destroy)
 
 def maske_trgt():
-    threading.Thread(target=yeniSayfa).start()
+    threading.Thread(target=new_page).start()
 def blur_trgt():
     threading.Thread(target=blur_def).start()
 def blur_trgt2():
     threading.Thread(target=blurring).start()
 def trgt2():
-    threading.Thread(target=Resim_Ekle).start()
+    threading.Thread(target=add_image).start()
 def trgt3():
-    threading.Thread(target=Kayıt).start()
+    threading.Thread(target=Record).start()
 
-B1 = Button(tk, text = "Resim Ekle", command=trgt2)
+def maissa():
+    img,lx,ly=readPGM(selected_image)
+    imgtk3=ImageTk.PhotoImage(bruit(img,lx,ly))
+    img,lx,ly=readPGM(selected_image)
+    L2 = Label(F1, image = imgtk3)
+    L2.image = imgtk3
+    L2.grid(row=1, column=1)
+    
+
+B1 = Button(tk, text = "Add Image", command=trgt2)
 B1.config(cursor="hand2")
 B1.place(x=180,y=450)
 
-hsv_btn = Button(tk,text="HSV Maskeleme", width = 13, command=maske_trgt)
+hsv_btn = Button(tk,text="HSV Masking", width = 13, command=maske_trgt)
 hsv_btn.config(cursor="hand2")
 hsv_btn.place(x=800,y=56)
 blur_btn = Button(tk,text="Bilateral Filter", width = 13,command=blur_trgt)
 blur_btn.config(cursor="hand2")
 blur_btn.place(x=800,y=90)
-hsv_btn = Button(tk,text="HSV Maskeleme", width = 13, command=maske_trgt)
+hsv_btn = Button(tk,text="HSV Masking", width = 13, command=maske_trgt)
 hsv_btn.config(cursor="hand2")
 hsv_btn.place(x=800,y=124)
-blur_btn = Button(tk,text="Bilateral Filter", width = 13,command=blur_trgt)
+blur_btn = Button(tk,text="maissa", width = 13,command=maissa)
 blur_btn.config(cursor="hand2")
 blur_btn.place(x=800,y=158)
 
 
-saveBTN = Button(tk, text = "Resmi Kayıt Et", command=trgt3)
+saveBTN = Button(tk, text = "Save Image", command=trgt3)
 saveBTN.config(state="disabled")
 saveBTN.place(x=565,y=450)
 
 def callback(url):
     webbrowser.open_new(url)
-me = Label(tk, text="Developer: yazilimfuryasi.com | @yazilimfuryasi", fg="#6E7371",cursor="hand2",font="Verdana 7 bold")
+me = Label(tk, text="Developers: Maissa Gallah - Chadha Siala", fg="#6E7371",cursor="hand2",font="Verdana 7 bold")
 me.place(x=280,y=485)
-me.bind("<Button-1>", lambda e: callback(webbrowser.open_new("https://www.instagram.com/yazilimfuryasi/")))
 
 tk.mainloop()
